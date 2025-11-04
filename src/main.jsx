97c553e238bd3189fa13e493e505d62b244013cd
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-11-04 09:49:29 +0300

    Replace Home component with RouterProvider for improved routing functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Home from "./components/Home";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "./routes/routes";
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </StrictMode>
 );
